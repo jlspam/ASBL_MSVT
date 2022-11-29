7d64d442d82c4622ceb5e0c0ff182885585e9361
Jerome <jlspamcon@gmail.com>
2022-11-29 13:02:14 +0100

    php convert

---
+++ Menu.php
@@ -0,0 +1,24 @@
+<nav class='Menu'>
+                <input type='checkbox' onclick='updatemenu()'>
+                    <ul>
+                        <li><a href='Main.html'>Accueil</a></li>
+                        <li><a class='dropdown-arrow' href='#'>Nos activités</a>
+                            <ul class='sub-menus'>
+                                <li><a href='Activites/Activite_sco.html'>Activités Scolaires</a></li>
+                                <li><a href='Activites/Actvite_ext.html'>Activités Extra-scolaires</a></li>
+                                <li><a href='Activites/Activite_vis.html'>Visites des carrières</a></li>
+                            </ul>
+                        </li>
+                        <li><a href='Res.html'>Réservations</a></li>
+                        <li><a href='Infos.html'>Infos pratiques</a></li>
+                        <li><a href='Contact.html'>Contact</a></li>
+                        <li><a href='Galerie.html'>Galerie</a></li>
+                        <li><a href='Newsletter'>Newsletter</a></li>
+                    </ul>
+                    <ul class="NavRight">
+                        <li><a href="Contact.html" target="blank" title="Contact"> <img src="pic/Icon/mail.png"></a></li>
+                        <li><a href="https://www.google.com/maps/place/ASBL+Maison+des+Sciences+de+la+Vie+et+de+la+Terre/@50.4756729,3.9985758,15z/data=!4m5!3m4!1s0x0:0xf65e28c2833d9182!8m2!3d50.4756793!4d3.9985818" target="blank" title="Map"> <img src="pic/Icon/map.png"></a></li>
+                        <li><a href="https://www.youtube.com/channel/UC6ej0-vJHTNis-bdzhBvzLg" target="blank" title="Youtube"><img src="pic/Icon/yt.png"></a></li>
+                        <li><a href="https://www.instagram.com/asbl_msvt/" target="blank" title="Instagram"><img src="pic/Icon/insta.png"></a></li>
+                    </ul>
+            </nav>
